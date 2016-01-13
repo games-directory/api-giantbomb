daac33b362911e3ac2b3c4a8c3869873ed5d91ad
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2016-01-13 16:13:39 +0000

    bump version

--- lib/giantbomb/version.rb
+++ lib/giantbomb/version.rb
@@ -1,3 +1,12 @@
 module GiantBomb
   VERSION = "1.5.5"
+
+  # Changelog
+  #
+  # v1.5.5 -
+  # v1.5.6 - Add Review resource
+  # v1.5.7 - Add User Review Resource
+  ## v1.5.8 - Planned
+  #
+
 end
